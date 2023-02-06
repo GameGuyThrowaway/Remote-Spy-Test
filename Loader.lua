d417e62643482198e1d51c587295d4d5ee57a0fd
GameGuyThrowaway <96356176+GameGuyThrowaway@users.noreply.github.com>
2023-02-05 18:30:43 -0800

    Update Loader.lua
    added functional loader

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,17 @@
-local mainRepo = 
+local loader = "https://raw.githubusercontent.com/GameGuyThrowaway/Synapse-V2-Remotespy/main/Loader.lua"
+
+local mainSourceFolder = "https://raw.githubusercontent.com/GameGuyThrowaway/Synapse-V2-Remotespy/main/Source/"
+
+local loadedModules = {}
+local function require(moduleName)
+    local module = loadedModules[moduleName]
+    if module then
+        return module
+    else
+        local newModule = loadstring(game:HttpGetAsync(mainSourceFolder .. moduleName), moduleName)()
+        loadedModules[moduleName] = newModule
+        return newModule
+    end
+end
+
+loadstring(game:HttpGetAsync(loader))(require) -- load core, passing require function
